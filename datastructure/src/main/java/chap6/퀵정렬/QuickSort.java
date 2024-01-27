26e463ab1ecb36bb58ecf5409fc9d27004f692da
ykmxxi <ykmxxi97@gmail.com>
2024-01-27 12:12:22 +0900

    feat: 자료구조와 알고리즘 6장 퀵 정렬 비재귀적 구현

--- datastructure/src/main/java/chap6/퀵정렬/QuickSort.java
+++ datastructure/src/main/java/chap6/퀵정렬/QuickSort.java
@@ -2,6 +2,8 @@ package chap6.퀵정렬;
 
 import java.util.Arrays;
 
+import chap4.stack.IntStack;
+
 public class QuickSort {
 
     static void quickSort(int[] arr, int left, int right) {
@@ -31,6 +33,45 @@ public class QuickSort {
         }
     }
 
+    // 비재귀적 퀵 정렬
+    static void quickSort2(int[] arr, int left, int right) {
+        IntStack leftStack = new IntStack(right - left + 1);
+        IntStack rightStack = new IntStack(right - left + 1);
+
+        leftStack.push(left);
+        rightStack.push(right);
+
+        while (!leftStack.isEmpty()) {
+            int pl = left = leftStack.pop();
+            int pr = right = rightStack.pop();
+            int x = arr[(left + right) / 2];
+
+            do {
+                while (arr[pl] < x) {
+                    pl++;
+                }
+                while (arr[pr] > x) {
+                    pr--;
+                }
+                if (pl <= pr) {
+                    swap(arr, pl, pr);
+                    pl++;
+                    pr--;
+                }
+            } while (pl <= pr);
+
+            if (left < pr) {
+                leftStack.push(left);
+                rightStack.push(pr);
+            }
+            if (pl < right) {
+                leftStack.push(pl);
+                rightStack.push(right);
+            }
+        }
+
+    }
+
     static void swap(int[] arr, int idx1, int idx2) {
         int tmp = arr[idx1];
         arr[idx1] = arr[idx2];
@@ -42,6 +83,11 @@ public class QuickSort {
         System.out.println("오름차순 퀵 정렬 전 = " + Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println("오름차순 퀵 정렬 후 = " + Arrays.toString(arr));
+
+        int[] arr2 = {1, 8, 7, 4, 5, 2, 6, 3, 9};
+        System.out.println("오름차순 퀵 정렬 전 = " + Arrays.toString(arr2));
+        quickSort2(arr2, 0, arr2.length - 1);
+        System.out.println("오름차순 퀵 정렬 후 = " + Arrays.toString(arr2));
     }
 
 }
